7884518a39b5a0246ef7b061cffe6c5c2e664f4b
Kristen Windoloski <163072677+KristenWindoloski@users.noreply.github.com>
2025-01-21 16:23:11 -0500

    Split larger functions into smaller subfunctions

--- R/ADME_outputfuncs.R
+++ R/ADME_outputfuncs.R
@@ -3,13 +3,53 @@
 # --- PLOT ALL ADME COMPOUND CURVES ON ONE PLOT
 ######################################################
 
+Create_Plotting_df <- function(i,n,sol_array,chemnames,numtimes){
+
+  # --- Create empty vectors
+  yvals = c()
+  compound_name_compartment = c()
+
+  # --- Create yvalue vector which includes the compartment data for all compounds
+  for (j in 1:n) {
+    yvals <- append(yvals, sol_array[,i+1,j])
+    compound_name_compartment <-append(compound_name_compartment, rep(chemnames[j],numtimes))
+  }
+
+  # --- Create data frame for state i to plot all state i curves at once
+  compartment_df <- data.frame(Time = sol_array[,1,1],
+                               Yvalues = yvals,
+                               Compound = compound_name_compartment)
+
+  compartment_df <- dplyr::arrange(compartment_df, Compound)
+  compartment_df$Compound <- factor(compartment_df$Compound, levels = unique(compartment_df$Compound))
+
+  return(compartment_df)
+}
+
+Set_Plot_Legend_Color <- function(plt_lst,n_states){
+
+  # --- Extract color pattern data frame for individual compound plots
+  plt_attrib <- ggplot2::ggplot_build(plt_lst[[n_states]])
+  plt_colors <- data.frame(colors = unique(plt_attrib[["data"]][[1]][["colour"]]))
+
+  # --- Save legend to plot later
+  plt_lst[[n_states+1]] <- cowplot::get_plot_component(plt_lst[[1]],"guide-box",return_all = TRUE)[[1]]
+
+  # --- Remove legend from current plots
+  for (j in 1:num_states) {
+    plt_lst[[j]] <- plt_lst[[j]] + ggplot2::theme(legend.position = "none")
+  }
+
+  out <- list(plt_lst, plt_colors)
+}
+
 plottingfunc_all <- function(sol_array){
 
   # --- Set needed variables; subtracted 1 from num_states because 'time' is one of the columns
   cn <- dimnames(sol_array)[2]
   col_names <- cn[[1]]
   num_states <- length(col_names) - 1
-  num_compounds <-dim(sol_array)[3]
+  num_compounds <- dim(sol_array)[3]
   n_times <- dim(sol_array)[1]
   dn <- dimnames(sol_array)[3]
   compound_names <- dn[[1]]
@@ -20,23 +60,7 @@ plottingfunc_all <- function(sol_array){
   # --- Generate plots for each compartment
   for (i in 1:num_states) {
 
-    # --- Create empty vectors
-    yvals = c()
-    compound_name_compartment = c()
-
-    # --- Create yvalue vector which includes the compartment data for all compounds
-    for (j in 1:num_compounds) {
-      yvals <- append(yvals, sol_array[,i+1,j])
-      compound_name_compartment <-append(compound_name_compartment, rep(compound_names[j],n_times))
-    }
-
-    # --- Create data frame for state i to plot all state i curves at once
-    compartment_df <- data.frame(Time = sol_array[,1,1],
-                                 Yvalues = yvals,
-                                 Compound = compound_name_compartment)
-
-    compartment_df <- dplyr::arrange(compartment_df, Compound)
-    compartment_df$Compound <- factor(compartment_df$Compound, levels = unique(compartment_df$Compound))
+    compartment_df <- Create_Plotting_df(i,num_compounds,sol_array,compound_names,n_times)
 
     # --- Plot curves for compartment i for all compounds
     plt_lst[[i]] <- ggplot2::ggplot(compartment_df, ggplot2::aes(Time, Yvalues, col = Compound)) +
@@ -46,27 +70,40 @@ plottingfunc_all <- function(sol_array){
       ggplot2::guides(col = ggplot2::guide_legend(ncol = 2))
   }
 
-  # --- Extract color pattern data frame for individual compound plots
-  plt_attrib <- ggplot2::ggplot_build(plt_lst[[num_states]])
-  plt_colors <- data.frame(colors = unique(plt_attrib[["data"]][[1]][["colour"]]))
-
-  # --- Save legend to plot later
-  # plt_lst[[num_states+1]] <- get_legend(plt_lst[[1]])
-  plt_lst[[num_states+1]] <- cowplot::get_plot_component(plt_lst[[1]],"guide-box",return_all = TRUE)[[1]]
-
-  # --- Remove legend from current plots
-  for (j in 1:num_states) {
-    plt_lst[[j]] <- plt_lst[[j]] + ggplot2::theme(legend.position = "none")
-  }
-
-  out_list <- list(plt_lst, plt_colors)
-
-  return(out_list)
+  # --- Outputs the plotting list and legend colors
+  out <- Set_Plot_Legend_Color(plt_lst,num_states)
 }
 
 ########################################################
 # --- PLOT ALL ADME COMPOUND CURVES ON INDIVIDUAL PLOTS
 ########################################################
+
+Create_Individ_Subplot <- function(comp_sol,compound_names,plt_colors,col_names){
+
+  compound_compartment_df <- data.frame(Time = comp_sol[,1],
+                                        Ydata = comp_sol[,j+1])
+
+  outplot <- ggplot2::ggplot(compound_compartment_df, ggplot2::aes(Time, Ydata, lty = compound_names[i])) +
+    ggplot2::geom_line(linewidth=1, color = plt_colors[i,1]) +
+    ggplot2::labs(x = "Time (Days)", y = col_names[j+1]) +
+    ggplot2::theme_bw() +
+    ggplot2::scale_linetype("Compound")
+}
+
+Set_Individ_Plot <- function(individ_plt_lst,n_states){
+
+  # --- Save legend to plot later
+  individ_plt_lst[[n_states+1]] <- cowplot::get_plot_component(individ_plt_lst[[1]],"guide-box",return_all = TRUE)[[1]]
+
+  # --- Remove legend from current plots
+  for (j in 1:n_states) {
+    individ_plt_lst[[j]] <- individ_plt_lst[[j]] + ggplot2::theme(legend.position = "none")
+  }
+
+  # --- Save all subplots for compound i to the ith entry of the master plot
+  # --- list for all compounds
+  out <- individ_plt_lst
+}
 
 plottingfunc_individual <- function(sol_array, plt_colors){
 
@@ -94,28 +131,11 @@ plottingfunc_individual <- function(sol_array, plt_colors){
 
     # --- Fill the list with each subplot for compound i
     for (j in 1:num_states) {
-
-      compound_compartment_df <- data.frame(Time = comp_sol[,1],
-                                            Ydata = comp_sol[,j+1])
-
-      individ_plt_lst[[j]] <- ggplot2::ggplot(compound_compartment_df, ggplot2::aes(Time, Ydata, lty = compound_names[i])) +
-        ggplot2::geom_line(linewidth=1, color = plt_colors[i,1]) +
-        ggplot2::labs(x = "Time (Days)", y = col_names[j+1]) +
-        ggplot2::theme_bw() +
-        ggplot2::scale_linetype("Compound")
+      individ_plt_lst[[j]] <- Create_Individ_Subplot(comp_sol,compound_names,plt_colors,col_names)
     }
 
-    # --- Save legend to plot later
-    individ_plt_lst[[num_states+1]] <- cowplot::get_plot_component(individ_plt_lst[[1]],"guide-box",return_all = TRUE)[[1]]
-
-    # --- Remove legend from current plots
-    for (j in 1:num_states) {
-      individ_plt_lst[[j]] <- individ_plt_lst[[j]] + ggplot2::theme(legend.position = "none")
-    }
-
-    # --- Save all subplots for compound i to the ith entry of the master plot
-    # --- list for all compounds
-    wholeplot_list[[i]] <- individ_plt_lst
+    # --- Set legend and final plot for compound i
+    wholeplot_list[[i]] <- Set_Individ_Plot(individ_plt_lst,num_states)
   }
 
   return(wholeplot_list)
@@ -139,7 +159,9 @@ plt_arrange <- function(plt_list){
 # --- GENERATE A TABLE WITH TK SUMMARY STATISTICS
 ########################################################
 
-TKsummary <- function(sol) {
+TKsummary <- function(modsol) {
+
+  sol <- as.data.frame(modsol, stringsAsFactors = FALSE)
 
   # --- Determine the number of states to be plotted (subtracted 1 because 'time' is one of the columns)
   col_names <- colnames(sol)
@@ -162,7 +184,8 @@ TKsummary <- function(sol) {
     TKsum[i,3] <- signif(max(sol[,(i+1)]),4)
     TKsum[i,4] <- signif(DescTools::AUC(x = sol[,'time'], y = sol[,col_names[i+1]], method = "trapezoid"),4)
   }
-  return(TKsum)
+
+  TKsum_final <- apply(as.matrix(TKsum[,2:4]), 2, as.numeric)
 }
 
 ########################################################
@@ -177,70 +200,105 @@ modsol <- function(pars){
   # --- Solve model for each compound
   for (i in (1:n)) {
 
-    modsolution <- httk::solve_model(chem.name = pars[["CompoundList"]][i,1],
-                                     route = pars[["doseroute"]],
-                                     input.units = pars[["doseunits"]],
-                                     dosing = pars[["dosinginfo"]],
-                                     species = pars[["spec"]],
-                                     model = pars[["model"]],
-                                     initial.values = pars[["initvals"]],
-                                     times = pars[["returntimes"]],
-                                     days = pars[["simtime"]],
-                                     method = pars[["odemethod"]],
-                                     tsteps = pars[["solversteps"]],
-                                     rtol = pars[["rtol"]],
-                                     atol = pars[["atol"]],
-                                     recalc.blood2plasma = pars[["rb2p"]],
-                                     restrictive.clearance = pars[["restrict_clear"]],
-                                     adjusted.Funbound.plasma = pars[["adj_fub"]],
-                                     minimum.Funbound.plasma = pars[["min_fub"]],
-                                     suppress.messages = TRUE,
-                                     parameterize.arg.list = list(default.to.human = pars[["defaulttoHuman"]],
-                                                                  regression = pars[["regression"]],
-                                                                  Caco2.options = list(Caco2.Pab.default = pars[["caco2default"]],
-                                                                                       Caco2.Fabs = pars[["caco_fabs"]],
-                                                                                       Caco2.Fgut = pars[["caco_fgut"]],
-                                                                                       overwrite.invivo = pars[["caco_overwriteinvivo"]],
-                                                                                       keepit100 = pars[["caco_keep100"]])))
+    modsolution <- Run_ADME_Model(pars)
 
-
-    #--- Set sizes of arrays
+    #--- Set sizes of output arrays
     if (i==1){
-      p <- nrow(modsolution)
-      q <- ncol(modsolution)
-
-      # --- sol is for storing the solution after its been plotted
-      sol <- array(data = rep(0,p*q*n), dim = c(p,q,n)) # (row, column, page)
-
-      # --- tk_sum_array is for storing the TK summary data (Tmax, Cmax, AUC)
-      tk_sum_array <- array(data = rep(0,(q-1)*3*n), dim = c((q-1),3,n))
+      arrs <- SetArraySize(modsolution,n)
+      sol <- arrs[[1]]
+      tk_sum_array <- arrs[[2]]
     }
 
-    # --- Save solution in array
+    # --- Save solution in array and generate TK Summary
     sol[,,i] <- modsolution
-
-    # --- Generate TK Summary (needs to be a data frame) and then save as an array
-    df_sol <- as.data.frame(modsolution, stringsAsFactors = FALSE)
-    df_tksum <- TKsummary(df_sol)
-    tk_sum_array[,,i] <- apply(as.matrix(df_tksum[,2:4]), 2, as.numeric)
-
+    tk_sum_array[,,i] <- TKsummary(modsolution)
   }
 
   # --- Assign row, column, and page names to the arrays
-  columns <- colnames(modsolution)
+  arr_out <- AssignArrayNames(modsolution,sol,pars)
+  sol <- arr_out[[1]]
+
+  # --- Rearrange TK summary array to be 2D data frame
+  tk_sum_array <- Rearr_TKSumArray(arr_out[[2]])
+
+  # --- Generate simulation parameters data frame
+  pars_df <- StorePars_ADME(pars)
+
+  # --- Create list with all outputs
+  out_list <- list(sol,tk_sum_array,pars_df)
+}
+
+Run_ADME_Model <- function(pars){
+
+  out <- httk::solve_model(chem.name = pars[["CompoundList"]][i,1],
+                           route = pars[["doseroute"]],
+                           input.units = pars[["doseunits"]],
+                           dosing = pars[["dosinginfo"]],
+                           species = pars[["spec"]],
+                           model = pars[["model"]],
+                           initial.values = pars[["initvals"]],
+                           times = pars[["returntimes"]],
+                           days = pars[["simtime"]],
+                           method = pars[["odemethod"]],
+                           tsteps = pars[["solversteps"]],
+                           rtol = pars[["rtol"]],
+                           atol = pars[["atol"]],
+                           recalc.blood2plasma = pars[["rb2p"]],
+                           restrictive.clearance = pars[["restrict_clear"]],
+                           adjusted.Funbound.plasma = pars[["adj_fub"]],
+                           minimum.Funbound.plasma = pars[["min_fub"]],
+                           parameterize.arg.list = list(default.to.human = pars[["defaulttoHuman"]],
+                                                        regression = pars[["regression"]],
+                                                        Caco2.options = list(Caco2.Pab.default = pars[["caco2default"]],
+                                                                             Caco2.Fabs = pars[["caco_fabs"]],
+                                                                             Caco2.Fgut = pars[["caco_fgut"]],
+                                                                             overwrite.invivo = pars[["caco_overwriteinvivo"]],
+                                                                             keepit100 = pars[["caco_keep100"]])))
+}
+
+SetArraySize <- function(modelsol,n){
+
+  # --- Find solution size
+  p <- nrow(modelsol)
+  q <- ncol(modelsol)
+
+  # --- sol is for storing the solution after its been plotted
+  sol <- array(data = rep(0,p*q*n), dim = c(p,q,n)) # (row, column, page)
+
+  # --- tk_sum_array is for storing the TK summary data (Tmax, Cmax, AUC)
+  tk_sum_array <- array(data = rep(0,(q-1)*3*n), dim = c((q-1),3,n))
+
+  out <- list(sol,tk_sum_array)
+}
+
+AssignArrayNames <- function(sol,pars){
+
+  columns <- colnames(sol)
   compartmentnames <- columns[2:length(columns)]
   compoundnames <- pars[["CompoundList"]][,1]
+
   dimnames(sol) <- list(c(), columns, compoundnames)
   statnames <- c("Tmax","MaxValue","AUC")
   dimnames(tk_sum_array) <- list(c(compartmentnames),statnames,compoundnames)
 
-  # --- Rearrange TK summary array to be 2D data frame
+  out <- list(sol,tk_sum_array)
+}
+
+Rearr_TKSumArray <- function(tk_sum_array){
+
+  statnames <- c("Tmax","MaxValue","AUC")
+  compoundnames <- pars[["CompoundList"]][,1]
+
   combdim <- list(outer(statnames,compoundnames, paste))
   mat_colnames <- gsub(" ", ".",combdim[[1]])
   dim_tk <- dim(tk_sum_array)
   tk_sum_array <- matrix(tk_sum_array,dim_tk[1],dim_tk[2]*dim_tk[3])
   dimnames(tk_sum_array) <- list(compartmentnames,mat_colnames)
 
+  return(tk_sum_array)
+}
+
+Dosing_Output <- function(pars){
 
   if (!is.null(pars[["dosinginfo"]]$initial.dose)){
     initial.dose <- pars[["dosinginfo"]]$initial.dose
@@ -261,14 +319,28 @@ modsol <- function(pars){
     dosing.matrix <- paste(pars[["dosinginfo"]]$dosing.matrix, collapse = ",")
   }
 
+  out <- list(initial.dose,doses.per.day,daily.dose,dosing.matrix)
+}
+
+Bind_Chem_Data <- function(pars,pars_df){
+
+  chemdata <- chem.physical_and_invitro.data[chem.physical_and_invitro.data$Compound %in% pars[["CompoundList"]][,1],]
+  chemdata <- chemdata[order(match(chemdata$Compound,pars_df$chem.name)),]
+  pars_df <- cbind(pars_df,chemdata)
+}
+
+StorePars_ADME <- function(pars){
+
+  dosinginfo <- Dosing_Output(pars)
+
   # --- Extract parameter values
   pars_df <- data.frame(chem.name = pars[["CompoundList"]][,1],
                         route = pars[["doseroute"]],
                         input.units = pars[["doseunits"]],
-                        dosing.initialdose = initial.dose,
-                        dosing.dosesperday = doses.per.day,
-                        dosing.dailydose = daily.dose,
-                        dosing.dosing.matrix = dosing.matrix,
+                        dosing.initialdose = dosinginfo[[1]],
+                        dosing.dosesperday = dosinginfo[[2]],
+                        dosing.dailydose = dosinginfo[[3]],
+                        dosing.dosing.matrix = dosinginfo[[4]],
                         species = pars[["spec"]],
                         model = pars[["model"]],
                         initial.values = paste(pars[["initvals"]],collapse = ","),
@@ -290,14 +362,6 @@ modsol <- function(pars){
                         overwrite.invivo = pars[["caco_overwriteinvivo"]],
                         keepit100 = pars[["caco_keep100"]])
 
-  chemdata <- chem.physical_and_invitro.data[chem.physical_and_invitro.data$Compound %in% pars[["CompoundList"]][,1],]
-  chemdata <- chemdata[order(match(chemdata$Compound,pars_df$chem.name)),]
-
-  pars_df <-cbind(pars_df,chemdata)
-
-  # --- Create list with all outputs
-  out_list <- list(sol,tk_sum_array,pars_df)
-
-  return(out_list)
+  # --- Bind parameter and chemical data
+  out <- Bind_Chem_Data(pars,pard_df)
 }
-
