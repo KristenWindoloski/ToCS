33ba3483128184ed9aea94d287df3c306e76ab9e
Kristen Windoloski Leonard <163072677+KristenWindoloski@users.noreply.github.com>
2025-04-24 16:17:25 -0400

    Updated global variables defined

--- inst/app/global.R
+++ inst/app/global.R
@@ -1,7 +1,17 @@
-#################################
-# GENERATE INITIAL CONDITIONS
-#################################
+
+###################################################
+# GENERATE INITIAL CONDITION GLOBAL VARIABLES
+###################################################
 
 ics <- names_ICs()
 ic_names <- ics[[1]]
 ic_comps <- ics[[2]]
+
+
+###################################################
+# GENERATE DATA GLOBAL VARIABLES
+###################################################
+
+physiology.data <- httk::physiology.data
+tissue.data <- httk::tissue.data
+chem.physical_and_invitro.data <- httk::chem.physical_and_invitro.data
