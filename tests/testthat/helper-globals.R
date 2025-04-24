0ae1d0560dc71216b90461efb9bfc3a1c4003128
Kristen Windoloski Leonard <163072677+KristenWindoloski@users.noreply.github.com>
2025-04-24 16:09:38 -0400

    Add httk data frames needed for tests

---
+++ tests/testthat/helper-globals.R
@@ -0,0 +1,9 @@
+
+physiology.data <- httk::physiology.data
+tissue.data <- httk::tissue.data
+chem.physical_and_invitro.data <- httk::chem.physical_and_invitro.data
+mecdt <- httk::mecdt
+mcnally_dt <- httk::mcnally_dt
+bmiage <- httk::bmiage
+wfl <- httk::wfl
+well_param <- httk::well_param
