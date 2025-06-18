35ba4837e765b876cc5829e646bfba68bcc6e871
Kristen Windoloski Leonard <163072677+KristenWindoloski@users.noreply.github.com>
2025-06-18 14:34:45 -0400

    Delete R/globals.R

--- R/globals.R
+++
@@ -1,7 +0,0 @@
-
-utils::globalVariables(names = c("ic_names",
-                                 "ic_comps"))
-
-utils::globalVariables(c('physiology.data',
-                         'tissue.data',
-                         'chem.physical_and_invitro.data'))
